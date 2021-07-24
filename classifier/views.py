174d3b1f8e08ada01e857e64db9c6d326b10a586
donikachristova <donika.christova@gmail.com>
2021-07-24 19:03:12 +1000

    No file uploaded error added

--- classifier/views.py
+++ classifier/views.py
@@ -5,6 +5,7 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse 
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from django.utils.datastructures import MultiValueDictKeyError
 
 #Fast AI imports
 import logging
@@ -29,26 +30,31 @@ def home(request):
 def upload(request):
     context = {}
     if request.method == 'POST':
-        if request.FILES['myfile']:
-            print("File")
-            print(request.FILES)
-            myfile = request.FILES['myfile']
-            print('RequestFile: ',myfile)
-            fs = FileSystemStorage()
-            print('FS: ',fs)
-            filename = fs.save(myfile.name, myfile)
-            print('FILENAME: ',filename)
-            uploaded_file_url = fs.url(filename)
-            print(uploaded_file_url)
-            classify_output = classify(filename)
-            print(classify_output)
-            context['colour'] = classify_output["classification"]
-            context['image_url'] = uploaded_file_url
-            context['confidence'] = '95'
-            return render(request, 'results.html', context)
-
-        else:
+        try:
+            if request.FILES['myfile']:
+                print("File")
+                print(request.FILES)
+                myfile = request.FILES['myfile']
+                print('RequestFile: ',myfile)
+                fs = FileSystemStorage()
+                print('FS: ',fs)
+                filename = fs.save(myfile.name, myfile)
+                print('FILENAME: ',filename)
+                uploaded_file_url = fs.url(filename)
+                print(uploaded_file_url)
+                # classify_output = classify(uploaded_file_url)
+                # print(classify_output)
+                # context['colour'] = classify_output["classification"]
+                context['image_url'] = uploaded_file_url
+                context['confidence'] = '95'
+                return render(request, 'results.html', context)
+            else:
+                raise
+        except MultiValueDictKeyError:
             context['error'] = 'No file uploaded'
+            return render(request, 'upload.html', context)
+        except:
+            context['error'] = 'Opps, something happened! Please try again.'
             return render(request, 'upload.html', context)
     else:
         return render(request, 'upload.html')
